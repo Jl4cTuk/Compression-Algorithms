888110e034e688b70b9abb7703d4e222cfdc8f14
Jl4cTuk <42464534+Jl4cTuk@users.noreply.github.com>
2022-11-09 09:09:29 +0000

    подсчитывает кол-во символов в тексте в двумерном массиве

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 file = open("Text.txt", "r")
 text = file.read()
 file.close()
-print('file:')
-print(text)
+#print(text)
+symbols = [0] * 127,[0] * 127
+for i in range(127):
+	symbols[0][i] = chr(i)
+for i in range(len(text)):
+	symbols[1][ord(text[i])]+=1
+
+print(symbols)
