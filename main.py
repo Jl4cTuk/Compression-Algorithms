83bd95dcbe3351d0d9e46d7cba5d2b2bb953f5e8
Jl4cTuk <42464534+Jl4cTuk@users.noreply.github.com>
2022-12-07 02:29:08 +0000

    всё удалил нафиг всю лабу, буду делать заново

--- main.py
+++ main.py
@@ -1,36 +1,5 @@
-#так как символы кодируются в utf-8, то русские буквы где-то там за тысячу и больше в таблице, поэтому пока только те символы, которые есть в ascii
-
-#функция отсортирует кол-во каждого символа по столбикам
-def sort(x):
-	for i in range(128):
-		for j in range(127):
-			if x[1][j]>x[1][j+1]:
-				tmp=x[1][j]
-				x[1][j]=x[1][j+1]
-				x[1][j+1]=tmp
-
-				tmp=x[0][j]
-				x[0][j]=x[0][j+1]
-				x[0][j+1]=tmp
-	return x
-
-#функция удалит символы которые используются ноль раз
-def removal(x):
-	while x[1][0]==0:
-			del x[1][0]
-			del x[0][0]
-	return x
-
-file = open("Text.txt", "r")
-text = file.read()
-file.close()
-symbols = [[0] * 128,[0] * 128]
-for i in range(128):
-	symbols[0][i] = chr(i)
-	
-for i in range(len(text)):
-	symbols[1][ord(text[i])]+=1
-
-sort(symbols)
-removal(symbols)
-print(symbols)
+print("[c]ompress/[d]ecompress?")
+arg1 = input()
+print("input file")
+arg2 = input()
+print(f"{arg1} {arg2}.enc")
