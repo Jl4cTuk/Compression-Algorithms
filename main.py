1279c850a8588ec632c3d11b0c43bfb4b1aba6e4
Jl4cTuk <42464534+Jl4cTuk@users.noreply.github.com>
2022-12-07 07:51:04 +0000

    начал делать нормальный код

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-print("[c]ompress/[d]ecompress?")
-arg1 = input()
-print("input file")
-arg2 = input()
-print(f"{arg1} {arg2}.enc")
+
+def symbols_counter(hex):
+    dict_sym = {}
+    for i in range(len(hex)):
+        dict_sym[chr(hex[i])] = dict_sym.get(chr(hex[i]), 0) + 1
+    return dict(sorted(dict_sym.items(), key=lambda item: item[1], reverse=False))
+
+def openbytes(myfile):
+	f = open(myfile, 'rb')
+	return f.read()
+
+def compress(myfile):
+	hex = openbytes(myfile)
+	dict_sym = symbols_counter(hex)
+	print(dict_sym)
+
+def decompress(myfile):
+	hex = openbytes(myfile)
+	print(f"{hex} decompressed")
+
+if __name__ == '__main__':
+	print("[c]ompress/[d]ecompress?")
+	arg1 = "c" #input()
+	print("input file")
+	arg2 = "Text.txt" #input()
+	if (arg1 == "c"):
+		compress(arg2)
+	elif (arg1 == "d"):
+		decompress(arg2)
+	else:
+		print("error!")
