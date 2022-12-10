d2c2f397450c87e6da5a0f4536386ce5385dca77
Jl4cTuk <42464534+Jl4cTuk@users.noreply.github.com>
2022-12-10 07:38:15 +0000

    строю древо

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
-
-def symbols_counter(hex):
+#создание словаря для с подсчитанными символами
+def symbols_counter(bytes):
     dict_sym = {}
-    for i in range(len(hex)):
-        dict_sym[chr(hex[i])] = dict_sym.get(chr(hex[i]), 0) + 1
+    for i in range(len(bytes)):
+        dict_sym[chr(bytes[i])] = dict_sym.get(chr(bytes[i]), 0) + 1
     return dict(sorted(dict_sym.items(), key=lambda item: item[1], reverse=False))
 
+#класс
+class Vertex:
+	def __init__(self, freq, symbol):
+		self.freq = freq
+		self.symbol = symbol
+		
+	def __lt__(self, other):
+		b = (self.freq <= other.freq)
+		return b
+
+#объндиняет буквы в древо
+def sort_leafs(dict_chars):
+	arr = []
+	for i in dict_chars:
+		arr.append(Vertex(dict_chars[i], i))
+	arr.sort()
+	return arr
+
+#чтение файла в байтах
 def openbytes(myfile):
 	f = open(myfile, 'rb')
 	return f.read()
 
+#супер функция которая сжимает
 def compress(myfile):
-	hex = openbytes(myfile)
-	dict_sym = symbols_counter(hex)
+	bytes = openbytes(myfile)
+	dict_sym = symbols_counter(bytes)
+	list_of_leafs = sort_leafs(dict_sym)
+	
 	print(dict_sym)
+	print(list_of_leafs)
 
+#суперская функция которая потом разожмёт
 def decompress(myfile):
 	hex = openbytes(myfile)
 	print(f"{hex} decompressed")
 
+#main
 if __name__ == '__main__':
 	print("[c]ompress/[d]ecompress?")
 	arg1 = "c" #input()
