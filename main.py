4d370016022844327927c99a9dc7ad95f25d156e
Jl4cTuk <42464534+Jl4cTuk@users.noreply.github.com>
2022-12-23 04:32:05 +0000

    доделал разархивировалку, почистил код, подготовил для сдачи

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import struct  #для разлиновывания байтов
+import struct
+import os
+
+def file_exists(filename):
+	filepath = os.path.join(filename)
+	return os.path.isfile(filepath)
 
 
-#класс создать объект с частотой и символом
 class Vertex:
 
 	def __init__(self, freq, symbol):
@@ -13,7 +17,6 @@ class Vertex:
 		return b
 
 
-#класс для дерева
 class TreeBuilder:
 
 	def __init__(self, right, left):
@@ -25,14 +28,10 @@ class TreeBuilder:
 		return self.freq <= other.freq
 
 
-#чтение файла в байтах
 def openbytes(myfile):
 	f = open(myfile, 'rb')
 	return f.read()
 
-
-#функции сжатия
-#создание словаря для с подсчитанными символами
 def symbols_counter(bytes):
 	dict_sym = {}
 	for i in range(len(bytes)):
@@ -40,7 +39,6 @@ def symbols_counter(bytes):
 	return dict_sym
 
 
-#упорядочивает буквы
 def sort_leafs(dict_chars):
 	arr = []
 	for i in dict_chars:
@@ -49,7 +47,6 @@ def sort_leafs(dict_chars):
 	return arr
 
 
-#распределение vertex объектов
 def tree_gen(arr):
 	while (len(arr) >= 2):
 		left = arr.pop(0)
@@ -61,7 +58,6 @@ def tree_gen(arr):
 	return root_node
 
 
-#создёет код хаффмана
 def huf_func(root, symbol_code='', huffman_code=dict()):
 	if type(root.right) is Vertex:
 		huffman_code[root.right.symbol] = symbol_code + '1'
@@ -74,7 +70,6 @@ def huf_func(root, symbol_code='', huffman_code=dict()):
 	return huffman_code
 
 
-#создаст текст из хаффмана
 def huf_text(bytes, huffman):
 	text = ''
 	for i in bytes:
@@ -83,7 +78,6 @@ def huf_text(bytes, huffman):
 	return text
 
 
-#супер функция из интернета которая чето там сделает мне крутое
 def rawbytes(letter):
 	outlist = []
 	for cp in letter:
@@ -99,8 +93,7 @@ def rawbytes(letter):
 	return b''.join(outlist)
 
 
-#буква и её кол-во
-def write_header(filename, dict_sym):
+def write_symbols(filename, dict_sym):
 	col_letters = (len(dict_sym.keys()) - 1).to_bytes(1, byteorder='little')
 	filename.write(col_letters)
 	for letter, code in dict_sym.items():
@@ -108,7 +101,6 @@ def write_header(filename, dict_sym):
 		filename.write(code.to_bytes(4, byteorder='little'))
 
 
-#из инета
 def get_byte_array(padded_encoded_text):
 	if (len(padded_encoded_text) % 8 != 0):
 		print("Encoded text not padded")
@@ -121,7 +113,6 @@ def get_byte_array(padded_encoded_text):
 	return b
 
 
-#паддинг текста по 8
 def pad_encoded_text(encoded_text):
 	extra_padding = 8 - len(encoded_text) % 8
 	for _ in range(extra_padding):
@@ -132,32 +123,50 @@ def pad_encoded_text(encoded_text):
 	return encoded_text
 
 
-#дописываем текст
 def write_text(file, enc_text):
 	enc_text_pad = pad_encoded_text(enc_text)
 	out = get_byte_array(enc_text_pad)
 	file.write(bytes(out))
 
 
-#архиватор
-def compress(myfile):
+def compress(myfile): #архивировалка
+	
+	#открыть файл
 	bytes = openbytes(myfile)
+	
+	#создание словаря
 	dict_sym = symbols_counter(bytes)
+	
+	#создаём вершины
 	vertex_arr = sort_leafs(dict_sym)
+	
+	#создание дерева и взятие корня
 	tree = tree_gen(vertex_arr)
+	
+	#создание кодов хаффмана
 	huffman = huf_func(tree)
+	
+	#создание двоичной последовательности текста
 	enc_text = huf_text(bytes, huffman)
 
+	#имя файла
 	file = f"{myfile}.huf"
+
+	#открыть новый файл
 	f = open(file, 'wb')
-	write_header(f, dict_sym)
+	
+	#запись символа и его количества
+	write_symbols(f, dict_sym)
+	
+	#запись двоичной последовательности в байты
 	write_text(f, enc_text)
+	
+	#закрыть
 	f.close()
-	print("Compressed")
 
 
 #чтение словаря
-def read_header(hex):
+def read_symbols(hex):
 	letter = hex[0]+1
 	header = hex[1:5*letter + 1]
 	dict_sym = dict()
@@ -165,32 +174,103 @@ def read_header(hex):
 		dict_sym[chr(header[i*5])] = int.from_bytes(header[i*5+1:i*5+5], byteorder='little')
 	return dict_sym
 
-#чтение символов
-def read_text(hex):
-	letter = hex[0]+1
-	enc_text = hex[5*letter + 1:]
-	return enc_text
+def read_text(input):
+	col_letters = input[0]+1
+	enc_text_pad = input[5*col_letters + 1:]
+	return enc_text_pad
 
-#антиархиватор ну или декомпрессионер
-def decompress(myfile):
-	hex = openbytes("text.txt.huf")
+def to_binary(enc_text):
+	result = ''
+	for i in enc_text:
+		bin_byte = bin(i)[2:].rjust(8, '0')
+		result += bin_byte
+	return result
 
-	dict_sym = read_header(hex)
-	enc_text = read_text(hex)
-	print(dict_sym)
-	print(enc_text.decode("utf-8"))
+def remove_padding(padded_text):
+	padded_info = padded_text[:8]
+	extra_padding = int(padded_info, 2)
+	padded_text = padded_text[8:]
+	binary_text = padded_text[:-1*extra_padding]
+	return binary_text
+
+def huf_to_text(bin_encoded_text, inv_huffman_codes):
+	text = ''
+	next_code = ''
+	for bit in bin_encoded_text:
+		next_code += bit
+		if (next_code in inv_huffman_codes.keys()):
+			text += inv_huffman_codes[next_code]
+			next_code = ''
+	return text
 	
-	#print("decompressed")
+def decompress(myfile): #разархивировалка
+	
+	#чтение байтов
+	hex = openbytes("text.txt.huf")
+	
+	#создание словаря
+	dict_sym = read_symbols(hex)
 
-#main
+	#создание вершин
+	vertex_arr = sort_leafs(dict_sym)
+	
+	#создание дерева
+	tree = tree_gen(vertex_arr)
+
+	#создание кодов хаффмана
+	huffman = huf_func(tree)
+
+	#чтение закодированного текста
+	enc_text = read_text(hex)
+
+	#переделывание в бинарьку
+	binary = to_binary(enc_text)
+
+	#убирание паддинга
+	binary_text = remove_padding(binary)
+
+	#меняем местами код и букву
+	#huffman = {v: k for k, v in huffman.items()}
+	#print(huffman)
+
+	inv_huffman = {v: k for k, v in huffman.items()}
+	#print(inv_huffman)
+	
+	#ну и всё, расшифровываем текст и GG
+	text = huf_to_text(binary_text, inv_huffman)
+
+	#расшифрованный файл
+	file = f"{myfile}"[:-4]
+
+	#файл
+	f = open(file, 'wb')
+
+	#запись
+	f.write(rawbytes(text))
+	
+	f.close()
+
 if __name__ == '__main__':
-	#print("[c]ompress/[d]ecompress?")
-	arg1 = "d"  #input()
-	#print("input file")
-	arg2 = "text.txt"  #input()
+	print("[c]ompress/[d]ecompress?")
+	arg1 = input()
+	
+	#проверка параметра
+	if not(arg1 == "c" or arg1 == "d"):
+		print("wrong parameter")
+		exit(1)
+		
+	print("input file")
+	arg2 = input()
+
+	#проверка файла
+	if not(file_exists(arg2)):
+		print("file not found!")
+		exit(1)
+
+	#вызов нужной функции
 	if (arg1 == "c"):
 		compress(arg2)
 	elif (arg1 == "d"):
 		decompress(arg2)
-	else:
-		print("error!")
+			
+	
